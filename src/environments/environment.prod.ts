2cf24bf2e15a6e7c25ff94fc5acd0827e6cce302
nasrii001 <nasri.mohammed001@gmail.com>
2019-05-25 22:19:50 +0200

    two

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,3 +1,11 @@
 export const environment = {
-  production: true
+  production: true,
+  firebase : {
+    apiKey: 'AIzaSyDPd8P_Mw8JbSSt6SkYUrh2dgDPxn1lGJ8',
+    authDomain: 'smart-ruche.firebaseapp.com',
+    databaseURL: 'https://smart-ruche.firebaseio.com',
+    projectId: 'smart-ruche',
+    storageBucket: 'smart-ruche.appspot.com',
+    messagingSenderId: '443407868089'
+  }
 };
